b87aabf43563ec11c07a38ea85e8b6d1b3d6b77e
Jeremiah <jeremiah@ideonic.com>
2016-07-18 17:49:18 +0800

    add register example

--- js/register.js
+++ js/register.js
@@ -3,7 +3,7 @@ var serverURL = 'http://localhost:3000/'
 
 $(function () {
   // listen for the form login
-  $('#login-form').on('submit', function (event) {
+  $('#register-form').on('submit', function (event) {
     event.preventDefault()
     var formData = $(this).serialize()
     signin(formData)
@@ -13,20 +13,17 @@ $(function () {
 function signin (formData) {
   $.ajax({
     type: 'POST',
-    url: serverURL + 'signin',
+    url: serverURL + 'signup',
     data: formData,
     success: function (response) {
-      // success save the repsonse
-      window.localStorage.email = $('#user-email').val()
-      window.localStorage.auth_token = response.auth_token
       // then redirect
-      window.location.href = 'about.html'
+      window.location.href = 'login.html'
     },
     error: function (xhr, ajaxOptions, thrownError) {
       // else output error
       console.log(xhr.status)
       console.log(thrownError)
-      window.alert('Login Failed')
+      window.alert('Singup Failed')
     }
   })
 }
