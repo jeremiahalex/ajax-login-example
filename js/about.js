771a37efd73ceac7a0da382698d2671d623f0bb5
Jeremiah <jeremiah@ideonic.com>
2016-07-18 17:52:30 +0800

    add logout example

--- js/about.js
+++ js/about.js
@@ -1,5 +1,13 @@
-/* globals currentUser */
+/* globals $ currentUser */
 function pageReady () {
   console.log('Hello ' + currentUser.name)
-}
 
+  // to logout we just clear the localstorage and redirect
+  $('#logout').click(function (event) {
+    event.preventDefault()
+
+    window.localStorage.removeItem('email')
+    window.localStorage.removeItem('auth_token')
+    window.location.href = 'login.html'
+  })
+}
